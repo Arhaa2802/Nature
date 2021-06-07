24b37e58bd15e7cf33287a8458b06b36420b163a
Arhaa2802 <84379843+Arhaa2802@users.noreply.github.com>
2021-06-07 12:40:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+function navigateF1(){//navigates to continents (forest)
+    window.location="forestC.html";
+};
+function navigate_home(){//navigates to home page
+    window.location="index.html";
+};
+function navigate_asian_forests(){//navigates to asian forests
+    window.location="asia_forest.html";
+};
+function navigate_europe_forests(){//navigates to european forests
+    window.location="europe_forest.html";
+};
+function navigate_northA_forests(){//navigate to North American Forests
+    window.location="NorthA_forest.html";
+}
+function navigate_southA_forests(){//navigate to South American Forests
+    window.location="SouthA_forest.html";
+}
+function navigate_Australia_forests(){//navigate to Australian Forests
+    window.location="Australia_forest.html";
+}
+function navigate_Africa_forests(){//navigate to African Forests
+    window.location="Africa_forest.html";
+}
+function navigateW2(){//navigates to continents (wildlife)
+    window.location="wildlifeC.html";
+}
+function navigate_asian_wildlife(){//navigates to Asia Wildlife
+window.location="asia_wildlife.html";
+}
+function navigate_europe_wildlife(){//navigates to Europe Wildlife
+    window.location="europe_wildlife.html";
+}
+    function navigate_northA_wildlife(){//navigates to North America Wildlife
+        window.location="northA_wildlife.html";
+}
+function navigate_southA_wildlife(){//navigates to South America Wildlife
+    window.location="southA_wildlife.html";
+}
+function navigate_Australia_wildlife(){//navigates to South America Wildlife
+    window.location="australia_wildlife.html";
+}
+function navigate_Africa_wildlife(){//navigates to Africa Wildlife
+    window.location="africa_wildlife.html";
+}
